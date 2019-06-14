8902dbd2b54fb00c54635ba4b6db327fff13692e
Chris Sun <sunhang0823@gmail.com>
2019-06-14 14:56:26 -0400

    login for an hour

--- src/main/java/com/hangsun/ppmtool/security/SecurityConstants.java
+++ src/main/java/com/hangsun/ppmtool/security/SecurityConstants.java
@@ -7,5 +7,5 @@ public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 600_000; //10 mins
+    public static final long EXPIRATION_TIME = 3600_000; //1 hour
 }
